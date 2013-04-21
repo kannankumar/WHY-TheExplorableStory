ec3f8040b1849cdc51038f85832eb8257501ffe2
Kannan Kumar <kannanthecurious@gmail.com>
2013-04-21 23:24:24 +0530

    Added Slide 4 of HTML5 book

---
+++ HTML5-explorable story book/Slide-4/Slide 4.hyperesources/slide4_hype_generated_script.js
@@ -0,0 +1,113 @@
+//	HYPE.documents["Slide 4"]
+
+(function HYPE_DocumentLoader() {
+	var resourcesFolderName = "Slide%204.hyperesources";
+	var documentName = "Slide 4";
+	var documentLoaderFilename = "slide4_hype_generated_script.js";
+	var mainContainerID = "slide4_hype_container";
+
+	// find the URL for this script's absolute path and set as the resourceFolderName
+	try {
+		var scripts = document.getElementsByTagName('script');
+		for(var i = 0; i < scripts.length; i++) {
+			var scriptSrc = scripts[i].src;
+			if(scriptSrc != null && scriptSrc.indexOf(documentLoaderFilename) != -1) {
+				resourcesFolderName = scriptSrc.substr(0, scriptSrc.lastIndexOf("/"));
+				break;
+			}
+		}
+	} catch(err) {	}
+
+	// Legacy support
+	if (typeof window.HYPE_DocumentsToLoad == "undefined") {
+		window.HYPE_DocumentsToLoad = new Array();
+	}
+ 
+	// load HYPE.js if it hasn't been loaded yet
+	if(typeof HYPE_160 == "undefined") {
+		if(typeof window.HYPE_160_DocumentsToLoad == "undefined") {
+			window.HYPE_160_DocumentsToLoad = new Array();
+			window.HYPE_160_DocumentsToLoad.push(HYPE_DocumentLoader);
+
+			var headElement = document.getElementsByTagName('head')[0];
+			var scriptElement = document.createElement('script');
+			scriptElement.type= 'text/javascript';
+			scriptElement.src = resourcesFolderName + '/' + 'HYPE.js?hype_version=160';
+			headElement.appendChild(scriptElement);
+		} else {
+			window.HYPE_160_DocumentsToLoad.push(HYPE_DocumentLoader);
+		}
+		return;
+	}
+	
+	// handle attempting to load multiple times
+	if(HYPE.documents[documentName] != null) {
+		var index = 1;
+		var originalDocumentName = documentName;
+		do {
+			documentName = "" + originalDocumentName + "-" + (index++);
+		} while(HYPE.documents[documentName] != null);
+		
+		var allDivs = document.getElementsByTagName("div");
+		var foundEligibleContainer = false;
+		for(var i = 0; i < allDivs.length; i++) {
+			if(allDivs[i].id == mainContainerID && allDivs[i].getAttribute("HYPE_documentName") == null) {
+				var index = 1;
+				var originalMainContainerID = mainContainerID;
+				do {
+					mainContainerID = "" + originalMainContainerID + "-" + (index++);
+				} while(document.getElementById(mainContainerID) != null);
+				
+				allDivs[i].id = mainContainerID;
+				foundEligibleContainer = true;
+				break;
+			}
+		}
+		
+		if(foundEligibleContainer == false) {
+			return;
+		}
+	}
+	
+	var hypeDoc = new HYPE_160();
+	
+	var attributeTransformerMapping = {b:"i",c:"i",bC:"i",d:"i",aS:"i",M:"i",e:"f",aT:"i",N:"i",f:"d",O:"i",g:"c",aU:"i",P:"i",Q:"i",aV:"i",R:"c",bG:"f",aW:"f",aI:"i",S:"i",bH:"d",l:"d",aX:"i",T:"i",m:"c",bI:"f",aJ:"i",n:"c",aK:"i",bJ:"f",X:"i",aL:"i",A:"c",aZ:"i",Y:"bM",B:"c",bK:"f",bL:"f",C:"c",D:"c",t:"i",E:"i",G:"c",bA:"c",a:"i",bB:"i"};
+	
+	var resources = {"0":{n:"science.jpg",p:1},"1":{n:"image2.png",p:1},"2":{n:"image24.png",p:1}};
+	
+	var scenes = [{x:0,p:"600px",c:"#FFFFFF",v:{"2":{o:"content-box",h:"0",x:"visible",a:0,q:"100% 100%",b:-36,j:"absolute",r:"inline",c:600,k:"div",z:"1",d:449},"3":{o:"content-box",h:"1",x:"visible",a:-183,q:"100% 100%",b:5,j:"absolute",r:"inline",c:173.43511450381678,k:"div",z:"2",d:400},"4":{o:"content-box",w:"",h:"2",x:"visible",a:197,q:"100% 100%",b:75,j:"absolute",r:"inline",c:96,k:"div",z:"3",d:214}},n:"Untitled Scene",T:{kTimelineDefaultIdentifier:{d:5.29,i:"kTimelineDefaultIdentifier",n:"Main Timeline",a:[{f:"2",t:0,d:2.29,i:"b",e:7,s:5,o:"3"},{f:"2",t:0,d:2.29,i:"a",e:-16,s:-183,o:"3"},{f:"2",t:0,d:5.29,i:"c",e:201,s:173.43511450381678,o:"3"},{f:"2",t:0,d:5.29,i:"d",e:464,s:400,o:"3"},{f:"2",t:2.29,d:3,i:"a",e:7,s:-16,o:"3"},{f:"2",t:2.29,d:3,i:"b",e:2,s:7,o:"3"}],f:30}},o:"1"}];
+	
+	var javascripts = [];
+	
+	var functions = {};
+	var javascriptMapping = {};
+	for(var i = 0; i < javascripts.length; i++) {
+		try {
+			javascriptMapping[javascripts[i].identifier] = javascripts[i].name;
+			eval("functions." + javascripts[i].name + " = " + javascripts[i].source);
+		} catch (e) {
+			hypeDoc.log(e);
+			functions[javascripts[i].name] = (function () {});
+		}
+	}
+	
+	hypeDoc.setAttributeTransformerMapping(attributeTransformerMapping);
+	hypeDoc.setResources(resources);
+	hypeDoc.setScenes(scenes);
+	hypeDoc.setJavascriptMapping(javascriptMapping);
+	hypeDoc.functions = functions;
+	hypeDoc.setCurrentSceneIndex(0);
+	hypeDoc.setMainContentContainerID(mainContainerID);
+	hypeDoc.setResourcesFolderName(resourcesFolderName);
+	hypeDoc.setShowHypeBuiltWatermark(0);
+	hypeDoc.setShowLoadingPage(false);
+	hypeDoc.setDrawSceneBackgrounds(true);
+	hypeDoc.setGraphicsAcceleration(true);
+	hypeDoc.setDocumentName(documentName);
+
+	HYPE.documents[documentName] = hypeDoc.API;
+	document.getElementById(mainContainerID).setAttribute("HYPE_documentName", documentName);
+
+	hypeDoc.documentLoad(this.body);
+}());
+
